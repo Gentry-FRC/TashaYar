8a2d1034023165e74d3827a797ef0ea696477640
Gabe Coones <lazerman595@gmail.com>
2020-12-22 22:43:32 -0600

    Upload for Archival
    Ooooh, shiny.

---
+++ src/org/usfirst/frc4843/Robot/commands/LightsCommand.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.usfirst.frc4843.Robot.commands;
+
+/**
+ *
+ * @author Developer
+ */
+public class LightsCommand {
+    
+}
---
+++ src/org/usfirst/frc4843/Robot/subsystems/Lights.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.usfirst.frc4843.Robot.subsystems;
+
+/**
+ *
+ * @author Developer
+ */
+public class Lights {
+    
+}
